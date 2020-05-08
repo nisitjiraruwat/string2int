30837e7f1af94c834443adae05657a0b921c65db
nisit.jiraruwat <nisit.jiraruwat@gmail.com>
2020-05-08 15:17:50 +0700

    Remove parseInt

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-// random input
-
 const string2int = (value) => {
-  let result = "";
-  let i;
-  for (i = 0; i < value.length; i++) {
+  let result = 0
+  let multiply = 1
+  let isInt = false
+  let i =  value.length
+  for (; 0 <= i; i--) {
     if (value[i] >= '0' && value[i] <= '9') {
-      result += value[i];
+      result += value[i] * multiply
+      multiply *= 10
+      isInt = true
     }
   }
 
-  return parseInt (result)
+  return isInt ? result : NaN
 }
 
+// random input
 const input = Math.floor((1 + Math.random()) * 0x1000000000).toString(16)
 
-console.log ('input: ' + input);
-console.log ('string2int: ' + string2int (input));
+console.log ('input: ' + input)
+console.log ('string2int: ' + string2int (input))
